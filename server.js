e76f49487c608b058dfeca09f75b4accbd0254a8
DAlper <da@feedgee.com>
2017-03-23 16:28:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-res.writeHead(200, {"Content-Type": "text/html"});
-res.write('<!DOCTYPE html>'+
-'<html>'+
-' <head>'+
-' <meta charset="utf-8" />'+
-' <title>My Node.js page!</title>'+
-' </head>'+ 
-' <body>'+
-' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
-' </body>'+
-'</html>');
-res.end();
-});
-server.listen(process.env.PORT || 3000);
-
-
-var GMT_UNIXTIME = function getCurrentTimeUTC()
-{
-    //RETURN:
-    //      = number of milliseconds between current UTC time and midnight of January 1, 1970
-    var tmLoc = new Date();
-    //The offset is in minutes -- convert it to ms
-    return tmLoc.getTime() + tmLoc.getTimezoneOffset() * 60000;
-};
-
-process.stdout.write('Unix Time: ' + GMT_UNIXTIME + ' Right?\n');
+var contentElement = $('#my-content');
+var addButtonElement = $('#add-content');
+var titleInput = $('#title-input');
+var bodyInput = $('#body-input');
+var contentTemplate = Handlebars.compile($('#entry-template').text());
+function addContent() {
+    var newData = contentTemplate({
+        title: titleInput.val(),
+        body: bodyInput.val()
+    });
+    contentElement.html(newData + contentElement.html());
+    // clean up inputs
+    titleInput.val('');
+    bodyInput.val('');
+}
+addButtonElement.on('click', addContent);
